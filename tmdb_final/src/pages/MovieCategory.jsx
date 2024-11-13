8b300f2f2f66262e24ffd1761db5075eced6c238
kangwonsik07 <alucard815@naver.com>
2024-11-13 18:05:24 +0900

    11-13

--- tmdb_final/src/pages/MovieCategory.jsx
+++ tmdb_final/src/pages/MovieCategory.jsx
@@ -14,6 +14,17 @@ function MovieCategory({ category }) {
    const dispatch = useDispatch()
    const { movies, loading, error } = useSelector((state) => state.movies)
 
+   /*
+     최초로 메뉴 클릭시  MovieCategory 컴포넌트 렌더링
+     이후 메뉴 클릭시 MovieCategory 컴포넌트는 재렌더링 X 
+
+      -> 같은 MovieCategory 컴포넌트를 사용하기 때문에 react-router-dom에서 경로가 바뀌어도 새로운 컴포넌트를 재렌더링 하지 X 
+      
+      -> 따라서 state는 메인 페이지에 있다가 최초로 메뉴중 하나를 클릭시 처음에만 1로 지정되고 이후 다른 메뉴 클릭시 1로 초기화 되지 않음
+      
+      -> 따라서 반드시 useEffect를 사용해 1로 초기화
+   */
+
    // 카테고리 별로 사용할 page 지정
    const [page, setPage] = useState({
       popular: 1,
@@ -23,25 +34,22 @@ function MovieCategory({ category }) {
 
    const isFirstLoad = useRef(true)
 
-   /* 
-      최초로 메뉴 클릭시 MovieCategory 컴포넌트 렌더링
-      메뉴 클릭시 MovieCategory 컴포넌트는 재렌더링 X 
+   /*
+     최초로 메뉴 클릭시 MovieCategory 컴포넌트 렌더링
+     이후 메뉴 클릭시 MovieCategory 컴포넌트는 재렌더링 X 
 
-      -> 같은 MovieCategory 컴포넌트를 사용하기 때문에 react-router-dom에서 경로가 바뀌어도 새로운 컴포넌트를 렌더링 하지 X
-      
-      -> 따라서 state는 최초로 메뉴중 하나를 클릭시 처음에만 1로 지정되고 이후 다른 메뉴 클릭시 1로 최기화 되지 않음
+      -> 같은 MovieCategory 컴포넌트를 사용하기 때문에 react-router-dom에서 경로가 바뀌어도 새로운 컴포넌트를 재렌더링 하지 X 
 
-      -> 따라서 반드시 useEffect를 사용해 1로 초기화 
+      -> 메인페이지에 있다가 최초로 메뉴를 클릭했을때는 MovieCategory 컴포넌트가 최초로 렌더링이 되면서 1번 useEffect와 2번 useEffect를 모두 실행한다.
 
-      -> 메인페이지에 있다가 최초로 메뉴를 클릭했을때는 1번 useEffectdhk 2번 useEffect를 모두 실행한다
+      -> 1번 useEffect에서 page 바뀜, 2번 useEffect API calling이 모두 일어나고 page가 바뀌는 현상때문에 2번 useEffect API calling이 한번 더 발생한다
 
-      -> 1번 useEffect에서 page 바꿈 2번 useEffect API calling이 모두 일어나고 page가 바뀌는 현상때문에 2번 useEffect API calling이 한번 더 발생한다
-
-      -> 이후 다른 메뉴 클릭시에는 category props만 바뀔 뿐 MovieCategory 컴포넌트가 재렌더링이 되지는 않으므로 API calling이 2번 되는 현상은 발생하지 X 
+      -> 이후 다른 메뉴 클릭시에는 category props만 바뀔 뿐 MovieCategory컴포넌트가 재렌더링이 되지는 않으므로 API calling이 2번 되는 현상은 발생하지 X
 
       -> 다만 해당 카테고리의 page state가 1로 바뀌면서 2번 useEffect가 한번 실행된다
 
       -> 따라서 useRef를 사용해 최초로 메뉴 클릭시에만 1번 useEffect를 실행하지 않도록 만들어준다.(어차피 최초로 메뉴 클릭시엔 page state는 모두 1)
+   
    */
 
    // 1번 useEffect
